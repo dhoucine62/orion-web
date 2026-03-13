478531ef8132628d1732a79643540cca58b30ea8
Houcine Drifi <drifihoucine62@gmail.com>
2026-03-13 08:52:35 +0100

    trie fichiers

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+
+    theme: {
+        extend: {
+            colors: {
+                aura: {
+                    green: '#00ff66',
+                }
+            }
+        }
+    }
+}
